40a5aa046747e278d324084a29437be078bfe2a4
SriramMurugappan <srirammurugappan.m@gavstech.com>
2023-09-21 14:45:42 +0530

    Book and book controller implemented

---
+++ DemoWebApi/BookDemo/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DemoWebApi.NewFolder
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+
+        public string AuthorName { get; set; }
+
+
+    }
+}
