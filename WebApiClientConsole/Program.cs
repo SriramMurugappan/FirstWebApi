be28016646dbd286707bd3f90a2cb7278312c68e
SriramMurugappan <srirammurugappan.m@gavstech.com>
2023-09-23 18:14:58 +0530

    EmployeeClient and viewModel Implemented

--- WebApiClientConsole/Program.cs
+++ WebApiClientConsole/Program.cs
@@ -2,14 +2,15 @@
 using WebApiClientConsole;
 
 Console.WriteLine("API CLient");
-EmpViewModel employeeToUpdate = new EmpViewModel();
-//{
-//    EmpId = 5,
-//    FirstName = "Update",
-//    LastName = "Update",
-//    City = "London",
-//    BirthDate = new DateTime(1960,01,01),
-//    HireDate = new DateTime(2000,01,01),
-//    Title = "Manager"
-//};
-EmployeeAPIClient.UpdateEmployee(5, employeeToUpdate).Wait();
+EmpViewModel employee = new EmpViewModel()
+{
+    EmployeeId = 15,
+    FirstName = "William",
+    LastName = "John Cooper",
+    City = "London",
+    BirthDate = new DateTime(1980, 01, 01),
+    HireDate = new DateTime(2000, 01, 01),
+    Title = "Manager"
+};
+//EmployeeAPIClient.UpdateEmployee(employee).Wait();
+EmployeeAPIClient.DeleteEmployee(11).Wait();
